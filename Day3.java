e6d880c7e39648b70e1c6c1579383a661bf39257
SitiMariati <mariatisiti34@gmail.com>
2025-09-10 19:34:23 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,29 @@
+ 
+package day3;
+
+
+public class Day3 {
+
+    
+    public static void main(String[] args) {
+        // untuk komentar ini, digunakan untuk komentar yang 1 baris saja
+        // Code di bawah ini akan menampilkan teks
+        System.out.println("Halo nama saya Siti");
+        
+        /* untuk komentar ini bisa digunakan untuk teks yang panjang 
+        atau sebuah paragraf penjelasan tentang code, jika kita membutuhkan
+        untuk penjelesan yang lebih banyak
+        */
+        
+        /* code di bawah ini menggunakan dua tipe data
+        yang pertama itu tipe data String, dan kedua adalah
+        tipe data int dan out put dari code dibawan ini akan 
+        menampilkan nama dan juga berat badan
+        */
+        String nama = "Siti";
+        int BB = 44;
+        System.out.println("Perkenalkan nama saya " + nama );
+        System.out.println("Dan berat badan saya " + BB + " kg");
+    }
+    
+}

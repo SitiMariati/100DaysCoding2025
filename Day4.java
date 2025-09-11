32bc1452226e6f9a90bb0e10c45da8e91f76f082
SitiMariati <mariatisiti34@gmail.com>
2025-09-11 17:52:08 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,21 @@
+package day4;
+
+public class Day4 {
+
+    public static void main(String[] args) {
+       
+        System.out.println("Halo nama saya\nSiti");
+        System.out.println("Day 4\nCoding 100 hari");
+        
+        
+        System.out.println("Nama :\tSiti Mariati");
+        System.out.println("Alamat :\tPasangkayu");
+        
+        System.out.println("Saya sedang\b coding");
+        
+        System.out.println("Folder code ini di simpan di : C:\\Usera\\Siti");
+        
+        
+    }
+    
+}

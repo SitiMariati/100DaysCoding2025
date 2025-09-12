fe6c110a72edd51c2a02b94c22a76366866dee00
SitiMariati <mariatisiti34@gmail.com>
2025-09-12 19:14:07 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,19 @@
+package day5;
+
+public class Day5 {
+
+    public static void main(String[] args) {
+        
+        byte S = 10;
+        int nilai =70;
+        short I = 90;
+        long T = 100;
+        
+        System.out.println(nilai);
+        System.out.println(S);
+        System.out.println(I);
+        System.out.println(T);
+
+    }
+    
+}

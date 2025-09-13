45d85016903738d8ccde315421714c923a207e9d
SitiMariati <mariatisiti34@gmail.com>
2025-09-13 19:11:09 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,12 @@
+package day6;
+
+public class Day6 {
+    public static void main(String[] args) {
+float BB = 44.0f;
+double TB = 154.1;
+
+        System.out.println(BB);
+        System.out.println(TB);
+    }
+    
+}

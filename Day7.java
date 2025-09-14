bfaadde2eddfaf5aa55d1559798d7bab996fb245
SitiMariati <mariatisiti34@gmail.com>
2025-09-14 15:33:16 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,13 @@
+package day7;
+
+public class Day7 {
+
+    public static void main(String[] args) {
+    char nilai = 'A';
+    boolean A = true;
+    
+        System.out.println(nilai);
+        System.out.println(A);
+    }
+    
+}

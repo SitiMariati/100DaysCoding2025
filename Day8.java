c8b02e36a2ba2f31e8a039dbc64c110ec8f200c5
SitiMariati <mariatisiti34@gmail.com>
2025-09-15 21:37:23 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,12 @@
+package day8;
+
+public class Day8 {
+
+    public static void main(String[] args) {
+        
+     String B = "Hai saya sedang ngoding hari ke 8";
+     
+        System.out.println(B);
+    }
+    
+}

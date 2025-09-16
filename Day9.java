9c39256499e5e3d1e4d01260b5f61e45dde764ef
SitiMariati <mariatisiti34@gmail.com>
2025-09-16 15:01:15 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,44 @@
+package day9;
+
+public class Day9 {
+
+    public static void main(String[] args) {
+        int A = 1;
+        A = 3;
+        
+        byte B = 3;
+        B =4;
+       
+        short C = 4;
+        C= 3;
+        long D = 6;
+        D =7;
+         float E = 9.0f;
+         E = 7.8f;
+         
+         double F = 3.2;
+         F = 9.3;
+         
+         char G = '%';
+         G= 'i';
+         
+         boolean H = true;
+         H = false;
+         
+         String Nama = "Siti";
+         Nama = "Pasangkayu";
+         
+         System.out.println(A);
+         System.out.println(B);
+         System.out.println(C);
+         System.out.println(D);
+         System.out.println(E);
+         System.out.println(F);
+         System.out.println(G);
+         System.out.println(H);
+         System.out.println(Nama);
+        
+        
+    }
+    
+}

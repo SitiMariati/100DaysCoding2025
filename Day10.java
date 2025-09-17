683f673023c99eb6d7725a40eebd96fb56bfe620
SitiMariati <mariatisiti34@gmail.com>
2025-09-17 18:36:52 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,11 @@
+package day10;
+
+public class Day10 {
+
+    public static void main(String[] args) {
+    // ketika kita menggunakan final, itu berarti variabel tersebut sdh tidak bisa kita update lagi
+         final int hari =7;
+        System.out.println(hari);
+   }
+    
+}

3290af33c615579a2cd7255363d6d37d556113bb
SitiMariati <mariatisiti34@gmail.com>
2025-09-18 19:54:31 +0800

    Create Day11.java

---
+++ Day11.java
@@ -0,0 +1,16 @@
+package day11;
+
+import java.util.Scanner;
+
+public class Day11 {
+    public static void main(String[] args) {
+        
+       Scanner input = new Scanner(System.in);
+        System.out.print( "Masukkan angka = ");
+        int angka = input.nextInt();
+        System.out.println("Angka Yang sudah di input = " + angka);
+        
+       
+    }
+    
+}

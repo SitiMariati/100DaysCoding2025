6da161e8f604feb231b8da034810457a0f44e8a2
SitiMariati <mariatisiti34@gmail.com>
2025-09-21 20:12:42 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,39 @@
+package day14;
+
+public class Day14 {
+    public static void main(String[] args) {
+            String nomor ="234";
+            int angka = Integer.parseInt(nomor);
+            
+            String nomor_2 ="34";
+            byte angka_2 = Byte.parseByte(nomor_2);
+            
+            String nomor_3 ="345";
+            short angka_3 = Short.parseShort(nomor_3);
+            
+            String nomor_4 ="456";
+            long angka_4 = Long.parseLong(nomor_4);
+            
+            String nomor_5 ="43";
+            float angka_5 = Float.parseFloat(nomor_5);
+            
+            String nomor_6 ="34";
+            double angka_6 = Double.parseDouble(nomor_6);
+             
+            String kondisi = "true";
+            boolean kondisi_1 = Boolean.parseBoolean(kondisi);
+            
+            String karakter = "A";
+            char karakter_1 = karakter.charAt(0);
+            
+            System.out.println(angka);
+            System.out.println(nomor_2);
+            System.out.println(nomor_3);
+            System.out.println(nomor_4);
+            System.out.println(nomor_5);
+            System.out.println(nomor_6);
+            System.out.println(kondisi_1);
+            System.out.println(karakter_1);
+    }
+    
+}

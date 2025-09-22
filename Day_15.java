5da0c830ea7716413efd4db6554040bd9d762a85
SitiMariati <mariatisiti34@gmail.com>
2025-09-22 17:49:40 +0800

    Create Day_15.java

---
+++ Day_15.java
@@ -0,0 +1,15 @@
+package day_15;
+
+public class Day_15 {
+    
+    public static void main(String[] args) {
+        int angka = 20;
+        int bilangan =10;
+        int penjumlahan = angka + bilangan;
+        int pengurangan = angka - bilangan;
+        System.out.println("Hasil penjumlahan = " + penjumlahan);
+        System.out.println("Hasil pengurangan = " + pengurangan);
+        
+  }
+    
+}

cbeecc6817685ea98cd18dba12ae48cb1e97dc2d
SitiMariati <mariatisiti34@gmail.com>
2025-09-23 18:43:50 +0800

    Create Day_16.java

---
+++ Day_16.java
@@ -0,0 +1,18 @@
+package day_16;
+
+ 
+public class Day_16 {
+
+    
+    public static void main(String[] args) {
+       int angka_1 = 12;
+       int angka_2 = 2;
+       
+       int perkalian = angka_1 * angka_2;
+       int pengurangan = angka_1 / angka_2;
+       
+        System.out.println("Hasil Perkalian = " + perkalian);
+        System.out.println("Hasil Pengurangan = " + pengurangan);
+    }
+    
+}

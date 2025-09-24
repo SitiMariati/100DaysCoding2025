e27882d4f7162433f5ba097e6f27915e5a23d9f7
SitiMariati <mariatisiti34@gmail.com>
2025-09-24 20:54:53 +0800

    Create Day_17.java

---
+++ Day_17.java
@@ -0,0 +1,14 @@
+package day_17;
+
+public class Day_17 {
+
+    public static void main(String[] args) {
+        int angka_1 = 10;
+        int angka_2 = 3;
+        
+        int sisa_bagi = angka_1 % angka_2;
+        
+        System.out.println(sisa_bagi);
+    }
+    
+}

4ef926b6fbfd72dc173f1bac4f3c9f0de20539bb
SitiMariati <mariatisiti34@gmail.com>
2025-09-28 22:00:04 +0800

    Create Day21.java

---
+++ Day21.java
@@ -0,0 +1,20 @@
+package day_21;
+
+public class Day_21 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("nilai A = " + a);
+        int b = 15;
+        System.out.println("nilai B = " + b);
+        
+       a = a + b; 
+       
+       b = a - b; 
+        System.out.println("nilai B = " + b);
+       a = a - b;
+        System.out.println("nilai A = " + a);
+        
+    }
+    
+}

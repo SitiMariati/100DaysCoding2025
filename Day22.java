ca3b2b870004ff959bd5d380d852ef3c7e4223ea
SitiMariati <mariatisiti34@gmail.com>
2025-09-29 21:34:28 +0800

    Create Day22.java

---
+++ Day22.java
@@ -0,0 +1,12 @@
+package day_22;
+
+public class Day_22 {
+
+    public static void main(String[] args) {
+      int sisi = 15;
+      int luas_persegi = sisi * sisi;
+      
+        System.out.println("Hasil Luas Persegi : " + luas_persegi);
+    }
+    
+}

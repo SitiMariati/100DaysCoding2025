bfa2010cbcaae8afabdcd419ceaa76fd2c1317c4
SitiMariati <mariatisiti34@gmail.com>
2025-09-30 17:19:16 +0800

    Create Day23.java

---
+++ Day23.java
@@ -0,0 +1,14 @@
+package day_23;
+
+public class Day_23 {
+
+    public static void main(String[] args) {
+       int panjang = 10;
+       int lebar = 15;
+       int Luas_persegi_panjang = panjang * lebar;
+       
+        System.out.println("Hasil luas persegi panjang :" + Luas_persegi_panjang);
+       
+    }
+    
+}

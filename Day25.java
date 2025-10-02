d1a2ee055c440e7ff58815a9da1901e5630da5ef
SitiMariati <mariatisiti34@gmail.com>
2025-10-02 21:43:23 +0800

    Create Day25.java

---
+++ Day25.java
@@ -0,0 +1,21 @@
+package day25;
+
+public class Day25 {
+
+    public static void main(String[] args) {
+        int a = 2;  
+        short b = 5;
+
+        System.out.println( "Ini adalah nilai A :" + (++a) );//Pre-increment (++a): 3
+        System.out.println("Ini adalah nilai A :" + (a--) );//Post-decrement (a--): 3
+        System.out.println("Ini adalah nilai A :" + (a) );// A : 2
+        System.out.println("Ini adalah nilai B :" + (++b) );//Pre-increment  (++b): 6
+        System.out.println("Ini adalah nilai B :" + (b--) ); //Post-decrement (b--): 6 
+        System.out.println("Ini adalah nilai B :" + (b) ); // B : 5
+        
+         
+        
+        
+  }
+    
+}

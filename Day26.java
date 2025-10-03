634a72195c9e1d3f28ecca674aed1f0d3933a4e8
SitiMariati <mariatisiti34@gmail.com>
2025-10-03 21:35:05 +0800

    Create Day26.java

---
+++ Day26.java
@@ -0,0 +1,25 @@
+        package pkg100_days;
+
+        public class Day26 {
+        public static void main(String[] args) {
+
+            int angka = 30; //penugasan = 30
+            System.out.println("Nilai angka : " + angka);
+            
+                angka *= 2; // mengalikan angka dengan 2 = 60
+                System.out.println("Nilai angka : " + angka);
+                
+                angka += 5; // menambahkan angka dengan 5 = 65
+                System.out.println("Nilai angka : " + angka);
+                
+                angka -= 5; // mengurangkan angka dengan 5 = 60
+                System.out.println("Nilai angka : " + angka);
+                
+                angka /= 5; // membagi angka dengan 5 = 
+                System.out.println("Nilai angka : " + angka);
+                
+                angka %= 5;
+                System.out.println("Nilai angka : " + angka);
+    }
+    
+}

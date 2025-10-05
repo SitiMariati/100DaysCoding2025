3eb4fa8bd214c090bbb21f595abf6a8985a90fa6
SitiMariati <mariatisiti34@gmail.com>
2025-10-05 20:44:37 +0800

    Create Day28.java

---
+++ Day28.java
@@ -0,0 +1,13 @@
+package pkg100_days;
+
+public class Day28 {
+
+    public static void main(String[] args) {
+        char karakter_1 = 'A';
+        char Karakter_2 = 'B';
+
+        System.out.println("Apakah Karakter_1 sama dengan Karakter_2 : " + (karakter_1 == Karakter_2));
+        System.out.println("Apakah Karakter_1 tidak sama dengan Karakter_2 : " + (karakter_1 != Karakter_2));
+    }
+
+}

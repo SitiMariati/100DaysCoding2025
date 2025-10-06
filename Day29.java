5b01f35ab0f0378ebc02fc76ced0dd48a0c3e05a
SitiMariati <mariatisiti34@gmail.com>
2025-10-06 21:04:48 +0800

    Create Day29.java

---
+++ Day29.java
@@ -0,0 +1,12 @@
+package pkg100_days;
+
+public class Day29 {
+    public static void main(String[] args) {
+        short angka_1 = 20;
+        short angka_2 = 15;
+        
+        System.out.println("Apakah Angka_1 lebih Kecil Dari Angka_2 : " + (angka_1 < angka_2));
+        System.out.println("Apakah Angka_1 lebih Besar Dari Angka_2 : " + (angka_1 > angka_2));
+    }
+    
+}

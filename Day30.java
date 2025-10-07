5d620a025fe6a8308bd84bcacda20aa44a415610
SitiMariati <mariatisiti34@gmail.com>
2025-10-07 17:59:02 +0800

    Create Day30.java

---
+++ Day30.java
@@ -0,0 +1,13 @@
+package pkg100_days;
+
+
+public class Day30 {
+    public static void main(String[] args) {
+        short angka = 25;
+        short angk_1 = 20;
+       
+        System.out.println(angka <= angk_1);
+        System.out.println(angka >= angk_1);
+    }
+    
+}

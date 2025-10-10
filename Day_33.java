9527a74ea352e5e0d11f0962a96aa74949872874
SitiMariati <mariatisiti34@gmail.com>
2025-10-10 20:56:57 +0800

    Create Day_33.java

---
+++ Day_33.java
@@ -0,0 +1,15 @@
+package pkg100_days;
+
+public class Day33 {
+    public static void main(String[] args) {
+
+boolean a = true;
+boolean b = false;
+
+        System.out.println("Boolean dari variabel A : " + !a);
+        System.out.println("Boolean dari variabel B : " + !b);
+       
+        
+    }
+    
+}

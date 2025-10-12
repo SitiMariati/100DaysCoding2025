f6f263949d84dfd2d351609933702270aeb33c64
SitiMariati <mariatisiti34@gmail.com>
2025-10-12 21:35:19 +0800

    Update Day35.java

--- Day35.java
+++ Day35.java
@@ -6,13 +6,13 @@ public class Day35 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         
-        System.out.print("Ketikkan Hari : ");
-        String hari = in.nextLine();
+        System.out.print("Ketikkan angka : ");
+        int angka = in.nextInt();
         
-        if(hari.equalsIgnoreCase("sabtu")  || hari.equalsIgnoreCase("Minggu")){
-            System.out.println("Hari libur");
+        if(angka > 0){
+            System.out.println("Bilangan positif");
         }else{
-            System.out.println("Hari kerja");
+            System.out.println("Bilangan negatif");
         }
     }
     

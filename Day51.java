b5202b1a19609cb8491bced8629bec62a1246e23
SitiMariati <mariatisiti34@gmail.com>
2025-10-28 19:09:47 +0800

    Create Day51.java

---
+++ Day51.java
@@ -0,0 +1,14 @@
+
+package pkg100_days;
+
+
+public class Day51 {
+    public static void main(String[] args) {
+        
+        for (int i = 1; i < 5; i++) {
+            System.out.println(i);
+            
+        }
+    }
+    
+}

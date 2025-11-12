d690643473ac5af06d3803c847a6cc26e2682fcd
SitiMariati <mariatisiti34@gmail.com>
2025-11-12 21:27:16 +0800

    Add Day66.java to print asterisks based on input

---
+++ Day66.java
@@ -0,0 +1,19 @@
+package pkg100_days;
+
+import java.util.Scanner;
+
+public class Day66 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Masukkan Angka N : ");
+        int N = in.nextInt();
+
+        for (int i = 1; i < N; i++) {
+            System.out.println("*");
+        }
+
+    }
+
+}

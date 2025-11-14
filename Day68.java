90f677d0e8f412376f84e3423eca1f4a50770e6c
SitiMariati <mariatisiti34@gmail.com>
2025-11-14 19:58:45 +0800

    Add Day68 class to print asterisks in a grid

---
+++ Day68.java
@@ -0,0 +1,13 @@
+
+package pkg100_days;
+
+public class Day68 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 6; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

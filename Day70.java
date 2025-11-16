e6ac104f4c52d3c0aa79d86bfd55ee497ba829b4
SitiMariati <mariatisiti34@gmail.com>
2025-11-16 22:04:43 +0800

    Add Day70 class to print asterisks in a grid

---
+++ Day70.java
@@ -0,0 +1,15 @@
+
+package pkg100_days;
+
+
+public class Day70 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 14; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
